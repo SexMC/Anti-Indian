9fbcef93b7da1313f4150efd5ed15bbd118c45ae
CJ <christophermustard@Christophers-MacBook-Air.local>
2025-04-02 12:10:13 -0700

    fuck pm

--- src/Main.php
+++ src/Main.php
@@ -7,6 +7,7 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener {
@@ -20,16 +21,13 @@ class Main extends PluginBase implements Listener {
         $locale = $player->getLocale();
         if ($locale == "hi_IN" || $locale == "bn_IN" || $locale == "ta_IN") { 
             $player->kick(TextFormat::RED . "You have been kicked by Anti-Indian\nPlease relocate to a first world country to continue.");
+            return;
         }
 
-        $task = new class($player) extends AsyncTask {
-            private string $addr;
+        $task = new class($player->getName(), $player->getNetworkSession()->getIp()) extends AsyncTask {
+            public function __construct(private string $username, private string $addr) { }
+
             private string $locale;
-
-            public function __construct(private Player $player) { 
-                $this->addr = $player->getNetworkSession()->getIp();
-            }
-
             public function onRun(): void {
                 $res = var_export(unserialize(file_get_contents("http://ip-api.com/php/". $this->addr)), true);
                 if (isset($res["countryCode"])) {
@@ -39,7 +37,8 @@ class Main extends PluginBase implements Listener {
 
              public function onCompletion(): void {
                 if ($this->locale == "IN" || $this->locale == "hi_IN" || $this->locale == "bn_IN" || $this->locale == "ta_IN") { 
-                    $this->player->kick(TextFormat::RED . "You have been kicked by Anti-Indian\nPlease relocate to a first world country to continue.");
+                    $player = Server::getInstance()->getPlayerExact($this->username);
+                    if (isset($player)) $player->kick(TextFormat::RED . "You have been kicked by Anti-Indian\nPlease relocate to a first world country to continue.");
                 }
              }
         };
