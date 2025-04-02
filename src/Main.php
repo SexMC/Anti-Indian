45ede5310c888e260347ab244ccfe792ed6466dc
CJ <christophermustard@Christophers-MacBook-Air.local>
2025-04-02 12:15:59 -0700

    pm aids

--- src/Main.php
+++ src/Main.php
@@ -19,7 +19,7 @@ class Main extends PluginBase implements Listener {
     public function onJoin(PlayerJoinEvent $playerJoinEvent): void {
         $player = $playerJoinEvent->getPlayer();
         $locale = $player->getLocale();
-        if ($locale == "hi_IN" || $locale == "bn_IN" || $locale == "ta_IN") { 
+        if (in_array($locale, ["hi_IN", "bn_IN", "ta_IN"])) { 
             $player->kick(TextFormat::RED . "You have been kicked by Anti-Indian\nPlease relocate to a first world country to continue.");
             return;
         }
@@ -27,16 +27,15 @@ class Main extends PluginBase implements Listener {
         $task = new class($player->getName(), $player->getNetworkSession()->getIp()) extends AsyncTask {
             public function __construct(private string $username, private string $addr) { }
 
-            private string $locale;
             public function onRun(): void {
                 $res = var_export(unserialize(file_get_contents("http://ip-api.com/php/". $this->addr)), true);
                 if (isset($res["countryCode"])) {
-                    $this->locale = $res["countryCode"];
+                    $this->setResult($res["countryCode"]);
                 }
              }
 
              public function onCompletion(): void {
-                if ($this->locale == "IN" || $this->locale == "hi_IN" || $this->locale == "bn_IN" || $this->locale == "ta_IN") { 
+                if ($this->getResult() == "IN") { 
                     $player = Server::getInstance()->getPlayerExact($this->username);
                     if (isset($player)) $player->kick(TextFormat::RED . "You have been kicked by Anti-Indian\nPlease relocate to a first world country to continue.");
                 }
